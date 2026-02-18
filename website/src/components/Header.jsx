765a096172c074bc5651fdbd857310ae97c80298
Zach Chen <zach.chen101@gmail.com>
2026-02-18 17:05:11 -0500

    made title and also made logo bigger

--- website/src/components/Header.jsx
+++ website/src/components/Header.jsx
@@ -5,7 +5,10 @@ function Header() {
     <header className="header">
       <div className="header-content">
         <img className="logo-img" src="/3.svg" alt="Cambridge Collider Logo" />
-        <h1 className="club-name">Cambridge Collider</h1>
+        <h1 className="club-name">
+          <span className="club-name-line club-name-cambridge">Cambridge</span>
+          <span className="club-name-line club-name-collider">Collider</span>
+        </h1>
       </div>
     </header>
   );
